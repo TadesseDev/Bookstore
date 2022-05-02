e6beb7768bc1e899224fbee7ca5812efce95bef6
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-02 21:11:54 +0300

    Create book reducer method

--- src/redux/books.js/books.js
+++ src/redux/books.js/books.js
@@ -2,3 +2,16 @@ const ADD_BOOK = 'ADD_BOOK';
 const REMOVE_BOOK = "REMOVE_BOOK";
 export const addBook = (title, author) => ({ type: ADD_BOOK, payload: { title, author } });
 export const removeBook = (id) => ({ type: REMOVE_BOOK, payload: id });
+export default (state = [], action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [
+        ...state,
+        action.payload
+      ]
+    case REMOVE_BOOK:
+      return state.filter((book, index) => index !== payload.id);
+    default:
+      state;
+  }
+};
