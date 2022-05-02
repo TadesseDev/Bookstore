82a811e36b35262cb26227ed93d0620c0408a3ad
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-02 22:41:10 +0300

    Fix linte warnings

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,7 +2,6 @@ import { createStore, combineReducers } from 'react-redux';
 import books from './books/books';
 import categories from './categories/categories';
 
-console.log(createStore)
 const rootReducer = combineReducers({
   books,
   categories,
