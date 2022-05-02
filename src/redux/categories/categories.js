fe8dbc475bf98026d7128be64ff96a36d3c1b61e
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-02 21:53:07 +0300

    Add educer for categories

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -6,3 +6,11 @@ export const checkStatus = () => ({ type: CHECK_STATUS });
 
 
 
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case CHECK_STATUS:
+      return 'Under construction';
+    default:
+      return state;
+  }
+}
