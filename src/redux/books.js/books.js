5814ad5d72aa00382c0dd997cea98ad85913a5c4
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-02 21:11:29 +0300

    Create book actions

--- src/redux/books.js/books.js
+++ src/redux/books.js/books.js
@@ -1,2 +1,4 @@
 const ADD_BOOK = 'ADD_BOOK';
 const REMOVE_BOOK = "REMOVE_BOOK";
+export const addBook = (title, author) => ({ type: ADD_BOOK, payload: { title, author } });
+export const removeBook = (id) => ({ type: REMOVE_BOOK, payload: id });
