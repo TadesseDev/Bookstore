306d49477f27547d1659b3e506547100d922ea7c
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-02 22:24:08 +0300

    Create root reducer

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,3 +2,8 @@ import { createStore, combineReducers } from 'Redux';
 import books from './books/books';
 import categories from './categories/categories';
 
+const rootReducer = combineReducers({
+  books,
+  categories,
+});
+
