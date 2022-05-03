2cefce6c8ad6e73d8d7976667be58df56818d184
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-03 20:23:59 +0300

    Update status text to the DOM

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
 
 export default function Categories() {
+  const dispatch = useDispatch();
+  const status = useSelector((state) => state.categories);
+  const handelCheckStatus = (event) => {
+    dispatch(checkStatus(event.target.getAttribute('id')));
+  };
+
   return (
     <div>
-      <button type="submit">Check status</button>
+      {status}
+      <button
+        type="submit"
+        id="CHECK_STATUS"
+        onClick={handelCheckStatus}
+      >
+        Check status
+      </button>
     </div>
   );
 }
