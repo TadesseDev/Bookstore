90b2483ff3fe8e41f98298e682c486824efbdd27
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-04 21:01:55 +0300

    Fetch intial book from API

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Book from './sub-components/book';
 import AddNewBook from './sub-components/AddNewBook';
+import { getBooksFromServer } from '../redux/books/books';
 
 export default function Books() {
+  const dispatch = useDispatch();
   const books = useSelector((store) => store.books);
+  useEffect(() => {
+    // console.log('data');
+    dispatch(getBooksFromServer());
+  }, []);
   return (
     <div>
       {books.map((book) => (
