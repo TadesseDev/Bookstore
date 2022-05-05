431cd5fb15c992dbb5af2b0a7d6a0ef8f96a8857
Tadesse-Alemayehu <tadalem26@gmail.com>
2022-05-05 10:06:03 +0300

    Create navbar style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './navbar.css';
 
 export default function Navbar() {
-  const navStyle = {
-    display: 'flex',
-    justifyContent: 'center',
-  };
-  const ulStyle = {
-    display: 'flex',
-    gap: '10px',
-    listStyle: 'none',
-  };
   return (
-    <nav style={navStyle}>
-      <ul style={ulStyle}>
+    <nav id="navbar">
+      <ul className="menuContainer">
         <li><NavLink to="/">Book</NavLink></li>
         <li><NavLink to="/categories">Category</NavLink></li>
       </ul>
