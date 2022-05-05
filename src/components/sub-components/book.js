d5525fb381a7e75e62913e954852b90e34dd9223
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-05 23:31:15 +0300

    Add book card style

--- src/components/sub-components/book.js
+++ src/components/sub-components/book.js
@@ -1,30 +1,35 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { removeBook } from '../../redux/books/books';
+import './books.css';
 
-export default function book({ title, author, id }) {
+export default function book({
+  title, author, id, category,
+}) {
   const dispatch = useDispatch();
   const handelRemoveBook = (event) => {
     dispatch(removeBook(event.target.getAttribute('id')));
   };
   return (
-    <article>
+    <article id="book">
       <div>
-        <h3>{title}</h3>
-        <small>{author}</small>
+        <p className="category">{category}</p>
+        <h2 className="title">{title}</h2>
+        <small className="author">{author}</small>
         <ul>
           <li><a href="#temp">Comment</a></li>
           <li><a href="#temp">Remove</a></li>
           <li><a href="#temp">Edit</a></li>
         </ul>
-        <button
-          type="submit"
-          id={id}
-          onClick={handelRemoveBook}
-        >
-          Remove this book
-        </button>
       </div>
+      <button
+        className="remove-book"
+        type="submit"
+        id={id}
+        onClick={handelRemoveBook}
+      >
+        Remove this book
+      </button>
     </article>
   );
 }
