d6ba2675fd8a247182f8728bd208784df31f2441
Tadesse-Alemayehu <tadalem26@gmail.com>
2022-05-06 10:07:29 +0300

    Comlete add new book component style

--- src/components/sub-components/AddNewBook.js
+++ src/components/sub-components/AddNewBook.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { addBook } from '../../redux/books/books';
+import './addBook.css';
 
 export default function AddNewBook() {
   const dispatch = useDispatch();
@@ -14,10 +15,14 @@ export default function AddNewBook() {
   };
   return (
     <div id="add-new-book">
+      <hr />
       <form action="#" method="POST" onSubmit={handleNewBookSubmit}>
-        <input type="text" name="title" id="title" placeholder="Book Title" required />
-        <input type="text" name="author" id="author" placeholder="Book author" required />
-        <button type="submit">Add Book</button>
+        <fieldset>
+          <legend>ADD NEW BOOK</legend>
+          <input className="book-title" type="text" name="title" id="title" placeholder="Book Title" required />
+          <input type="text" name="author" id="author" placeholder="Book author" required />
+          <button id="add-book-button" type="submit">Add Book</button>
+        </fieldset>
       </form>
     </div>
   );
